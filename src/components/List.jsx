f22414100ec7de278bc08e868e9157b77f841c1b
DezBooM <dezboom3@gmail.com>
2023-01-02 13:38:16 +0100

    CSS improvements

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,14 +3,14 @@ import TodoList from "./TodoList"
 
 export default function List({ filtered, handleDelete, handleComplete }) { return (
         <div className="list">
-            <ul>
+            <ul className={filtered.length === 0 ? "noShadow" : ""}>
             {filtered.length > 0 ? filtered.map(todo => {
                 return <TodoList 
                             key={todo.id} 
                             {...todo} 
                             handleDelete={() => handleDelete(todo.id)}
                             handleComplete={() => handleComplete(todo.id)} />
-            }) : <p>No todo found</p>}
+            }) : <p className="noTodo">No todo found.</p>}
             </ul>
         </div>
     )
